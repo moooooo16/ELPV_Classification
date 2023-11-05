53103c7740219658f3751a12b0a6e180ea586506
Mo-Mac <limodesert@gmail.com>
2023-11-04 23:39:43 -0400

    bovw complte

--- README.md
+++ README.md
@@ -20,4 +20,50 @@ Use `pip install -e .` to install the package in editable mode.
 # Findings
 - Data is heavily unbalanced
 
+# Results
 
+K : 64 -> 1024, dense -> sparse
+
+K = 64
+KNN: 0.67, 0.63
+SVM: 0.68, 0.64
+RF: 0.68, 0.65
+LogReg: 0.65, 0.62
+Tree: 0.61, 0.63
+adaB: 0.59, 0.60
+GdtB: 0.68, 0.62
+
+K = 128
+KNN: 0.66, 
+SVM: 0.69
+RF: 0.66
+
+K = 256
+KNN: 0.65
+SVM: 0.68
+RF: 0.67
+
+K = 384
+KNN: 0.61
+SVM: 0.70
+RF: 0.64
+
+K = 512
+KNN: 0.57
+SVM: 0.71
+RF: 0.65
+
+K = 640
+KNN: 0.52
+SVM: 0.66
+RF: 0.67
+
+K = 768
+KNN: 0.44
+SVM: 0.68
+RF: 0.68
+
+K = 896
+KNN: 0.42
+SVM: 0.71
+RF: 0.68
--- elpv/utils/classifier.py
+++ elpv/utils/classifier.py
@@ -1,25 +1,35 @@
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.model_selection import GridSearchCV
 
-def grid_search(X_train, y_train, X_test, y_test, estimator, params, cv, scoring):
+def grid_search(X_train, y_train, X_test, y_test, estimator, params, cv, scoring, verbose=3, n_jobs=-1, return_train_score=True):
     clf = GridSearchCV(estimator, params,
                                cv=cv,
                                scoring=scoring,
-                               verbose=3,
-                               n_jobs=-1)
+                               verbose=verbose,
+                               n_jobs=n_jobs,
+                               return_train_score=return_train_score)
     clf.fit(X_train, y_train)
     
     best_clf = clf.best_estimator_
+    print(f'Best Score: {clf.best_score_} use {clf.best_params_}')
     
+    result = clf.cv_results_
+    
+    # Test prediction
     pred = best_clf.predict(X_test)
-    report = classification_report(y_test, pred, zero_division=1)
+
+    acc, conf_mat = get_report(y_test, pred)
+        
+    
+    return result, best_clf, pred, acc, conf_mat
+
+def get_report(y_test, pred):
+    report = classification_report(y_test, pred, zero_division=0)
     acc = accuracy_score(y_test, pred)
     conf_mat = confusion_matrix(y_test, pred)
     
-    print(f'Best params: {clf.best_params_}')
-    print('Classification report:')
+    print('Testset Classification Report:')
     print(report)
-    print('-'*50)
     print()
     
-    return best_clf, pred, acc, conf_mat
+    return  acc, conf_mat
